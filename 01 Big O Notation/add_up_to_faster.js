d53d8fe9ef8dcd398d8b4b5c1d26924581a9bb5d
Mr-Smoke-7 <kumroshan120@gmail.com>
2022-03-16 22:48:31 +0530

    O Notation faster

--- 01 Big O Notation/add_up_to_faster.js
+++ 01 Big O Notation/add_up_to_faster.js
@@ -1,8 +1,10 @@
 function addUpTo(n) {
-  return n * (n + 1) / 2;
+  // return (n * (n + 1)) / 2;
+  return n  / 2;
 }
 
 var time1 = performance.now();
 addUpTo(1000000000);
 var time2 = performance.now();
-console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+console.log(time1, time2);
+console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`);
