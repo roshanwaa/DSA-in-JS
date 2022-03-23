1b24f96efde987a4db8668621410c561b5abe3cc
Mr.Smoke <kumroshan120@gmail.com>
2022-03-24 01:00:52 +0530

    Prob 1 Ques add

--- 04 Problem Solving Approach/prob1.js
+++ 04 Problem Solving Approach/prob1.js
@@ -1,3 +1,23 @@
+// ! Q. Write a function which takes in a string and returns counts of each character in the string.
+// charCount('"Your PIN number is 1234!")
+/* {
+   1: 1,
+   2: 1,
+    3: 1,
+   4: 1,
+    b: 1,
+    e: 1,
+   i: 2,
+    m: 1,
+   n: 2,
+    o: 1,
+   p: 1,
+    r: 2,
+    s: 1,
+    u: 2,
+   у: 1
+} */
+
 function charCount(str) {
   // * make object to return at end
   var result = {};
@@ -18,4 +38,4 @@ function charCount(str) {
   return result;
 }
 
-charCount('Hello, world! this is Mr.Smoke');
+console.log(charCount('Hello, world! this is Mr.Smoke'));
