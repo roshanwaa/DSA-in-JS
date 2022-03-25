33d555fb2ca8022370a8bed5c5f753ae4199fed3
Mr.Smoke <kumroshan120@gmail.com>
2022-03-25 20:48:10 +0530

    Add If else Statement

--- 04 Problem Solving Approach/prob1.js
+++ 04 Problem Solving Approach/prob1.js
@@ -18,24 +18,42 @@
    у: 1
 } */
 
+// function charCount(str) {
+//   // * make object to return at end
+//   var result = {};
+//   // * loop over string, for each character...
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str[i];
+//     //* if the char is a number/letter AND is a key in object, add one to count
+//     if (result[char] > 0) {
+//       result[char]++;
+//     }
+//     //* if the char is a number/letter AND not in object, add it to object and set value to 1
+//     else {
+//       result[char] = 1;
+//     }
+//   }
+//   //* if character is something else (space, period, etc.) don't do anything
+//   // * return object at end
+//   return result;
+// }
+
+// ! Or
+
 function charCount(str) {
-  // * make object to return at end
-  var result = {};
-  // * loop over string, for each character...
+  var obj = {};
   for (var i = 0; i < str.length; i++) {
-    var char = str[i];
-    //* if the char is a number/letter AND is a key in object, add one to count
-    if (result[char] > 0) {
-      result[char]++;
-    }
-    //* if the char is a number/letter AND not in object, add it to object and set value to 1
-    else {
-      result[char] = 1;
+    var char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+        1;
+      }
     }
   }
-  //* if character is something else (space, period, etc.) don't do anything
-  // * return object at end
-  return result;
+  return obj;
 }
 
 console.log(charCount('Hello, world! this is Mr.Smoke'));
