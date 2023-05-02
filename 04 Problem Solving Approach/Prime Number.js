09ae5654173d8b3da6ef560fb21bc3806899aae9
Mr.Smoke <kumroshan120@gmail.com>
2023-05-02 23:30:21 +0530

    Sqrt of priime no.

--- 04 Problem Solving Approach/Prime Number.js
+++ 04 Problem Solving Approach/Prime Number.js
@@ -12,6 +12,26 @@ const isPrime = (n) => {
 
 console.log(isPrime(1)); // returns false
 console.log(isPrime(5)); // returns true
-console.log(isPrime(4)); // returns false
+console.log(isPrime(4) + '\n'); // returns false
 
 // Big O = O(n)
+
+// ============ OR ============
+
+function isPrimeSQRT(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrimeSQRT(1)); // returns false
+console.log(isPrimeSQRT(5)); // returns true
+console.log(isPrimeSQRT(4)); // returns false
+
+// Big O = O(n^2)
