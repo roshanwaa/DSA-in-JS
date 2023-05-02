7a7680a9fdc9f292364334eb0aae26eee7566eca
Mr.Smoke <kumroshan120@gmail.com>
2023-05-02 21:27:58 +0530

    fibonacci Sequance

--- 04 Problem Solving Approach/fibonacciSequance.js
+++ 04 Problem Solving Approach/fibonacciSequance.js
@@ -1,30 +1,12 @@
-// const fibonacci = (n) => {
-//   const fibonacciSequence = (0, 1])
-//   for (let i = 2; i < n; i++) {
-//     fibonacciSequence(i] = )ibonacciSequence(i - 1) + fibonacciSequence(i - 2);
-//   }
-//   return fibonacciSequence;
-// };
-
-// console.log(fibonacci(2));
-// console.log(fibonacci(3));
-// console.log(fibonacci(7));
-// console.log(fibonacci(15));
-
-const fibonacciNum = (n) => {
-  if (n == 0 || n == 1) {
-    return n;
+const fibonacci = (n) => {
+  const fibonacciSequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
   }
-
-  return fibonacciNum(n - 1) + fibonacciNum(n - 2);
+  return fibonacciSequence;
 };
 
-//   public:
-//     int fib(int n)
-// 	{
-//         if(n==0 or n==1){return n;}
-//        return  fib(n-2)+fib(n-1);
-//          }
-// };
-
-console.log(fibonacciNum(2));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
+console.log(fibonacci(15));
