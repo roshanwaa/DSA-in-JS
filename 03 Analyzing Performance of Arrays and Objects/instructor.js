1d99863dee5d17ee90852625b009c5ff390a91d7
Mr.Smoke <kumroshan120@gmail.com>
2023-05-02 19:30:13 +0530

    array and object BIG O

--- 03 Analyzing Performance of Arrays and Objects/instructor.js
+++ 03 Analyzing Performance of Arrays and Objects/instructor.js
@@ -5,18 +5,47 @@ let instructor = {
 };
 console.log(instructor.firstName);
 
-/*
- ! Big O of Objects
- * Insertion -   O(1)
- * Removal -   O(1)
- * Searching -   O(N)
- * Access -   O(1)
- */
+//  ! Big O of Objects
+//  * Insertion -   O(1)
+//  * Removal -   O(1)
+//  * Searching -   O(N)
+//  * Access -   O(1)
+//  */
+
+// /*
+//  ! Big O of Object Methods
+//  * Object.keys -   O(N)
+//  * Object.values -   O(N)
+//  * Object.entries -   O(N)
+//  * hasOwnProperty -   O(1)
+// */
+
+// ! ARRAYS
+//*Ordered lists!
+
+let names = ['Michael', 'Melissa', 'Andrea'];
+
+let values = [true, {}, [], 2, 'awesome'];
+
+// ? Big O of Arrays
+/* 
+  Insertion -   It depends....
+  Removal -   It depends....
+  Searching -   O(N)
+  Access -   O(1)
+  Let's see what we mean by that!
+*/
+
+// ! Big O of Array Operations
 
 /* 
- ! Big O of Object Methods
- * Object.keys -   O(N)
- * Object.values -   O(N)
- * Object.entries -   O(N)
- * hasOwnProperty -   O(1) 
+  push -   O(1)
+  pop -   O(1)
+  shift -   O(N)
+  unshift -   O(N)
+  concat -   O(N)
+  slice -   O(N)
+  splice -   O(N)
+  sort -   O(N * log N)
+  forEach/map/filter/reduce/etc. -   O(N) 
 */
