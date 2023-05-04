c1b7c0a6e229070c2de27c16601207442a38f15d
Mr.Smoke <kumroshan120@gmail.com>
2023-05-04 22:52:33 +0530

    isPowerOfTwoBitsWise Optimized power of two O(1)

--- 04 Problem Solving Approach/PowerofTwo.js
+++ 04 Problem Solving Approach/PowerofTwo.js
@@ -14,4 +14,21 @@ function powerOfTwo(n) {
 console.log(powerOfTwo(1)); // true
 console.log(powerOfTwo(2)); // true
 console.log(powerOfTwo(5)); // false
-console.log(powerOfTwo(8)); // true
+console.log(powerOfTwo(8) + '\n'); // true
+
+// Big O = O(logN)
+
+// ============= Optimized implementation =============
+
+function isPowerOfTwoBitsWise(n) {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwoBitsWise(2));
+console.log(isPowerOfTwoBitsWise(5));
+console.log(isPowerOfTwoBitsWise(8));
+
+// Big O = O(1)
